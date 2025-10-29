98b4bd9dc2cec3f905b0bc506f31e7667754837f
Thuy Trang Skalska <skalskathuytrang@gmail.com>
2025-10-29 10:14:24 +0100

    etc

---
+++ vars.py
@@ -0,0 +1,35 @@
+a = 56235365427864189461796514753651927647127367312964713246
+print(f'a={type(a)}')
+f = 632874652379.4731289641329786413278923
+print(f'f={f}')
+g = 0.0000000000000000000000000000054325232353
+print(f'g={g}')
+
+
+f1 = 6.328746523794731289641329786413278923e11
+print(f'f1={f1}')
+
+max_num = 1.79e308
+print(f'max_num={type(max_num)}')
+inc_max_num = max_num*10
+print(f'inc_max_num={inc_max_num}')
+
+b = 10.342
+print(f'b={b}')
+c = int(b)
+print(f'c={c}')
+d = round(b)
+print(f'd={d}')
+e = float(d)
+print(f'e={e}')
+print(f'f=(e) = {type(e) }')
+sn = '2526.786agsg'
+print(f'sn={sn}')
+print(f'type(sn) = {type(sn) }')
+sn_f=float(sn)
+print(f'type(sn_f) = {type(sn) }')
+
+b1=0b1011
+print(f'b1={b1}')
+x1=0xf1
+print(f'x1={x1}')
